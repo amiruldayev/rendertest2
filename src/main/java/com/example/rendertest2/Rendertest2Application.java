62b1fd94a68ee0ffaed74fdd0af354e2df389cf6
amiruldayev <amiruldaeve@gmail.com>
2023-11-29 17:22:29 +0600

    first

---
+++ src/main/java/com/example/rendertest2/Rendertest2Application.java
@@ -0,0 +1,13 @@
+package com.example.rendertest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rendertest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rendertest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rendertest2/Rendertest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rendertest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rendertest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
